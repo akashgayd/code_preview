1e94c9ec3ddae566882815b55078c26f02ffdfbe
unknown <akashgaydhane781@gmail.com>
2025-03-08 11:01:12 +0530

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
         <Routes>
 
             <Route path="/" element={<HomePage />} />
-            <Route path="/code-review" element={<CodePreview />} />
+            <Route path="/" element={<CodePreview />} />
             
 
 
